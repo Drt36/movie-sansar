7a46a873005aca4c1a4ea252701a44dd75039d5f
Dharma Raj Thanait <dharmarajthanait25@gmail.com>
2021-09-19 01:06:00 +0545

    Slider Top for upcoming movies completed

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Footer from '../Footer/Footer'
 import Navbar from '../Navbar/Navbar'
+import SliderTop from '../Slider/SliderTop'
 
 export const Home = () => {
     return (
         <div>
             <Navbar/>
+            <SliderTop/>
             <Footer/>
         </div>
     )
